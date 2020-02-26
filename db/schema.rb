2d9b85823c4e32379985a9ce023e13f86789801e
Sam Moss <sam.moss321@gmail.com>
2020-02-26 14:58:04 -0800

    adds staff array to practice table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_194605) do
+ActiveRecord::Schema.define(version: 2020_02_26_225429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_194605) do
     t.string "name", null: false
     t.jsonb "contact", default: "{}", null: false
     t.bigint "users_id"
+    t.text "staff", default: [], array: true
     t.index ["users_id"], name: "index_practices_on_users_id"
   end
 
