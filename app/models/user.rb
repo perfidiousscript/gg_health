7a330692b1474813361b07f0f9a2ce8f99943671
Sam Moss <sam.moss321@gmail.com>
2020-02-26 14:57:45 -0800

    fixes typo in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   enum role: [ :consumer, :practitioner, :manager, :admin ]
-  has_many: :practices
-  has_many: :locations, through: :practices
+  has_many :practices
+  has_many :locations, through: :practices
 end
