d7293c8bcbca8105a70d5ff048fe72117358be39
Sam Moss <sam.moss321@gmail.com>
2020-02-26 13:38:19 -0800

    switches db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_192718) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.integer "role", default: 0, null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
