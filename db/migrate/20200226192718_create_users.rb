a3acd4f8c5ded55b5a75657f80bfef24e791fd4d
Sam Moss <sam.moss321@gmail.com>
2020-02-26 11:32:04 -0800

    adds users table

---
+++ db/migrate/20200226192718_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.column :role, :integer, default: 0, :null -> false
+    end
+  end
+end
