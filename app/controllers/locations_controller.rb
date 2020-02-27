bd65d22f6169bac302b2e6c153e6daa40fc062c9
Sam Moss <sam.moss321@gmail.com>
2020-02-27 14:54:27 -0800

    adds location indexing by practice and practice indexing by user

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,3 +1,10 @@
 LocationsController < ApplicationController
-  
+  def index
+    if params[:practice_id].present?
+      @locations = Practice.find_by(params[:practice_id]).locations
+    else
+      @locations = Locations.all
+    end
+    render :json @locations
+  end
 end
--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,3 +1,10 @@
 PracticesController < ApplicationController
-
+  def index
+    if params[:user_id].present?
+      @practices = User.find_by(params[:user_id]).practices
+    else
+      @practices = Practices.all
+    end
+    render :json @practices
+  end
 end
