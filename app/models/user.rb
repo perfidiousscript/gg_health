c1d1e0eb849862d30d9a13f6f3d63ff2cf6590fa
Sam Moss <sam.moss321@gmail.com>
2020-02-28 11:37:51 -0800

    work on users controller and model to allow lat/long

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   enum role: [ :consumer, :practitioner, :manager, :admin ]
   has_many :practices
   has_many :locations, through: :practices
+  geocoded_by :coordinates
+
+  def coordinates
+    [latitude, longitude].compact.join(', ')
+  end
+
 end
