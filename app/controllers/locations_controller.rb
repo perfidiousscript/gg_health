9ae957f6a103abe9930f7e56303ad56af7ff3008
Sam Moss <sam.moss321@gmail.com>
2020-02-28 12:03:30 -0800

    adds create method on locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,22 @@
 class LocationsController < ApplicationController
+
+  def create
+    @Practice = Practice.find(params[:practice_id])
+
+    if @Practice
+      @Location = Location.new(location_params)
+    else
+      render json: {error: "Practice not found."}
+    end
+
+    if @Location.save
+      render json: @Location, status: :created
+    else
+      render json: {error: "Could not save location."}
+    end
+
+  end
+
   def index
     if params[:id].present?
       @locations = Practice.find(params[:id]).locations
@@ -6,5 +24,9 @@ class LocationsController < ApplicationController
       @locations = Location.all
     end
     render json: @locations
+  end
+
+  def location_params
+      params.permit(:name, :address, :phone_number, :contact, :services, :practice_id, :staff, :latitude, :longitude)
   end
 end
