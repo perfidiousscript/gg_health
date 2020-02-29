166e7aae8b1d040a8b5c348784b6059daea520c6
Sam Moss <sam.moss321@gmail.com>
2020-02-28 16:41:39 -0800

    allows location search to search on service tags

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -30,11 +30,18 @@ class LocationsController < ApplicationController
 
     @search_distance = params[:search_distance] || 10
 
-    if params[:latitude] != nil and params[:longitude]
+    if params[:latitude] != nil and params[:longitude] !=nil and params[:services] != nil
+      @locations = Location.near([params[:latitude],params[:longitude]], @search_distance).where('services @> ?', {services:params[:services]}.to_json)
+    elsif
       @locations = Location.near([params[:latitude],params[:longitude]], @search_distance)
     else
       render json: {"error":"Location data missing!"}
       return
+    end
+
+    if params[:services] != nil
+      @unfiltered_locations = @locations
+      @locations = @unfiltered_locations
     end
 
     render json: @locations
