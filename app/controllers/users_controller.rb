8eed90252fa32219f2fb1eb1df768d84fe0c3f6e
Sam Moss <sam.moss321@gmail.com>
2020-03-02 21:29:43 -0800

    adds update action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render status: :internal_server_error
+    end
+
+  end
+
   def show
     @user = User.find(params[:id])
 
