db4c469245bce039bff4121b8f23aa70fac80f39
Sam Moss <sam.moss321@gmail.com>
2020-03-25 15:43:22 -0700

    adds outline for services hash to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,21 @@ class Location < ApplicationRecord
   geocoded_by :coordinates
   after_validation :geocode
 
+  # Services JSONB xould look something like this:
+  # With primary service (if applicable) then
+  # 'sub services' represented in hashes
+  # {
+  #  "primary_service": "massage",
+  #  "massage":   [
+  #              "standard",
+  #              "deep_tissue",
+  #              "shiatsu"
+  #             ]
+  #  "reiki":   [
+  #              "standard"
+  #              ]
+  # }
+
   def coordinates
     [latitude, longitude].compact.join(', ')
   end
