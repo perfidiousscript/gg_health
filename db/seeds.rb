20a1ee3387d92ba64249f5d409e9d96a70fd8f64
Sam Moss <sam.moss321@gmail.com>
2020-03-26 12:41:10 -0700

    adds password to db/seeds and changes Practice contact format

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@ User.delete_all
 
 user_list = [
   ["Melinda", "Manager", "melinda.manager@fake.com", 2],
-  ["Patricia", "Practitioner", "patricia.pracitioner@fake.com", 2],
+  ["Patricia", "Practitioner", "patricia.pracitioner@fake.com", 1],
   ["Carrie", "Client", "carrie@fake.com", 0]
 ]
 
 user_list.each do |first_name, last_name, email_address, role|
-  User.create(first_name: first_name, last_name: last_name, email_address: email_address, role: role)
+  User.create(first_name: first_name, last_name: last_name, email_address: email_address, role: role, password: '12345')
 end
 
 melinda = User.find_by_first_name("Melinda")
 patty = User.find_by_first_name("Patricia")
 
 practice_list = [
-  ["Melindas Massages",{"phone":"18005322345"},[0,1]]
+  ["Melindas Massages",[{type:"phone", value:"18005322345"}],[0,1]]
 ]
 
 practice_list.each do |name, contact, staff|
@@ -31,6 +31,5 @@ location_list = [
 ]
 
 location_list.each do | name, address, phone_number, contact, services, staff|
-  byebug
   melinda.practices.first.locations.create(name: name, address: address, phone_number: phone_number, contact: contact, services: services, staff: staff)
 end
