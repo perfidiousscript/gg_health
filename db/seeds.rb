fbdab0e1eb364c48e5f6affae4b2c15a0709c66b
Sam Moss <sam.moss321@gmail.com>
2020-03-30 14:43:21 -0700

    updates seeds to generate twenty locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,16 @@ User.delete_all
 
 user_list = [
   ["Melinda", "Manager", "melinda.manager@fake.com", 2],
+  ["Manny", "Manager", "manny.manager@fake.com", 2],
   ["Patricia", "Practitioner", "patricia.pracitioner@fake.com", 1],
   ["Carrie", "Client", "carrie@fake.com", 0]
 ]
+
+portland = Geocoder.search("Portland, OR")
+
+bounding_box =  portland[0].data["boundingbox"]
+
+services_list = ["massage", "reiki", "float tank", "matrix energetics", "tarot"]
 
 user_list.each do |first_name, last_name, email_address, role|
   User.create(first_name: first_name, last_name: last_name, email_address: email_address, role: role, password: '12345')
@@ -26,10 +33,20 @@ end
 
 melindas_practice = Practice.find_by_name("Melindas Massages")
 
-location_list = [
-  ["Melindas Massages","345 98th St Portland, OR", "18005322345", {}, {"services": "massage"}, [melinda.id,patty.id]]
-]
+# id: integer, name: string, address: string, phone_number: string, contact: jsonb, services: jsonb, practice_id: integer, staff: text, latitude: float, longitude: float
 
-location_list.each do | name, address, phone_number, contact, services, staff|
-  melinda.practices.first.locations.create(name: name, address: address, phone_number: phone_number, contact: contact, services: services, staff: staff)
+location_list = []
+
+address = ''
+phone_number = '4755843948'
+contact = {"type":"", "value":""}
+staff = [0,1]
+
+20.times do |i|
+  service = services_list.sample
+  name = "Melinda's #{service} #{i}"
+  lat = rand(bounding_box[0].to_f..bounding_box[1].to_f)
+  long = rand(bounding_box[2].to_f..bounding_box[3].to_f)
+  location = melinda.practices.first.locations.create(name: name, address: address, phone_number: phone_number, contact: contact, services: service, latitude: lat, longitude: long, staff: staff)
+  location_list.push(location)
 end
