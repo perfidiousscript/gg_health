77e5e2d2d4d898691cf0b408186fec850dfbd203
Sam Moss <sam.moss321@gmail.com>
2020-04-29 13:12:24 -0700

    sets Location Calendar row default to empty hash

--- db/migrate/20200414163550_add_calendar_to_location.rb
+++ db/migrate/20200414163550_add_calendar_to_location.rb
@@ -1,5 +1,5 @@
 class AddCalendarToLocation < ActiveRecord::Migration[6.0]
   def change
-    add_column :locations, :calendar, :jsonb
+    add_column :locations, :calendar, :jsonb, default: {},
   end
 end
